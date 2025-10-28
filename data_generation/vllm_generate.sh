b77cc19e986fe658a2cede3492aee85a2c46ea8b
Ashish Magadum <shonenash@gmail.com>
2025-10-28 17:06:56 -0400

    modify script, remove email

--- data_generation/vllm_generate.sh
+++ data_generation/vllm_generate.sh
@@ -8,21 +8,24 @@
 #SBATCH --ntasks=1
 #SBATCH --output=vllm_llama_%j.out
 #SBATCH --error=vllm_llama_%j.err
-#SBATCH --mail-user=magadum.a@northeastern.edu
+#SBATCH --mail-user=<YOUR_NORTHEASTERN_EMAIL>
 #SBATCH --mail-type=ALL
 
 #modules
 module load anaconda3/2024.06
 module load cuda/12.8.0
 
+# change to workspace dir
+cd ~/workspace
+
 #python env
-source /home/magadum.a/workspace/.venv/bin/activate
+source .venv/bin/activate
 
 unset http_proxy https_proxy
 
 echo "running python script"
 #code
-python /home/magadum.a/workspace/generate_vllm.py
+python generate_vllm.py
 
 # capture exit code
 EXIT_CODE=$?
